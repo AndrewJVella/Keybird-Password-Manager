79406c3ad704b0678995c4e6ebb8a7134e77c7f7
PixelatedStarfish <68627998+PixelatedStarfish@users.noreply.github.com>
2022-11-30 20:30:58 -0500

    Update trinket_source.py

--- trinket_source.py
+++ trinket_source.py
@@ -1,10 +1,7 @@
-#on trinket here: https://trinket.io/features/python3
-from datetime import date
 import random
-import traceback
+import replit
 import os
-import base64
-import subprocess
+import getpass
 # PRAGRAPH FORMAT INDICATORS
 #^ is newline
 
@@ -14,7 +11,6 @@ import subprocess
 def main(d):
 
 	print("Ready.\n----")
-	print("Thank you for using the Keybasket 2.0 Web Application Repl.\nCopyright 2022.\n\nThis is created by Andrew Vella.\nEmail:  andyjvella@gmail.com.\nGithub: @PixelatedStarfish\n\nPlease see the Disclaimer and License before using this app.") 
 	menu()
 	return
 
@@ -22,8 +18,13 @@ def main(d):
 def menu():
 	global KEY_LENGTH
 	global PASSWORD_LENGTH
+	global MASK
 	while (True):
-		a = input("\nType a number to select an option:\n0  Help\n1  One Key Mode\n2  Two Key Mode\n3  Username Generator\n4  See Disclaimer and License\n> ")
+		replit.clear()
+		print("Thank you for using the Keybird Web Repl.\nCopyright 2022.\n\nThis is created by Andrew Vella.\nEmail:  andyjvella@gmail.com.\nGithub: @PixelatedStarfish\n\nPlease see the Disclaimer and License before using this app.") 
+
+    
+		a = input("\nType a number to select an option:\n0  Help\n1  One Key Mode\n2  Two Key Mode\n3  Username Generator\n4  Toggle Key Mask\n5  See Disclaimer and License\n> ")
 		if (a == "0"):
 			print(paragraphAlign("^-Help-^^This application is designed to generate\nand store passwords on your computer.\nWhile other managers integrate with your browser,\nthey are much better for accumulating passwords\nthan actually managing them.\nAt worst you can become\ndependant on a specific browser to access your stuff.\nAt best you might still use\na small number of passwords\nyou wrote yourself for the important stuff,\nwhich puts the important stuff at risk.") + "\n\n" + paragraphAlign("My thinking is that 100 auto-generated passwords is bad,\na small number of manually created passwords is worse,\nand both of these together is what often happens.\nInstead of coming up with a few compilcated\npasswords and using them everywhere,\ndepending on a browser extension,\nor (most likely) both\nThis generates secure passwords from simple keys,\nthat are easy to create and remember.") + "\n\n" + paragraphAlign("Keys are "+ str(KEY_LENGTH) +" characters max.^Passwords are " + str(PASSWORD_LENGTH) + " characters."))
 		if (a == "1"):
@@ -33,18 +34,33 @@ def menu():
 		if (a == "3"):
 			genUserName()
 		if (a == "4"):
+			MASK = not MASK
+			if (MASK):
+				print("Mask is on. Input will be hidden.")
+			if (not MASK):
+				print("Mask is off. Input will not be hidden.")
+		if (a == "5"):
 			print("-Disclaimer-\n")
 			print(paragraphAlign("So, in plain terms, this app should be used with some care. It is about as secure as a basket of keys. If you keep it behind a locked door, your keys should stay put because only trusted people get anywhere near them. By analogy this app is the basket, and your computer is the door. You should be sure that your computer is secured and safe before using this app. No information produced in whole or part by this app is garenteed to be perfectly safe. Your files can be read, modified, or deleted. The app source code can also be read, modfied, or deleted, which would cause unpredictable effects while running the app. Back up important things; store your passwords in a few safe places. Keep untrustworthy people off your computer. Do not store your passwords publicly, or generate them with keys that are easy to guess. Stay safe!"))
 			print("\n\n")
 			print(paragraphAlign('-MIT License-^^Copyright (c) 2022 Andrew Vella^^Permission is hereby granted, free of charge, to any person obtaining a copy^of this software and associated documentation files (the "Software"), to deal^in the Software without restriction, including without limitation the rights^to use, copy, modify, merge, publish, distribute, sublicense, and/or sell^copies of the Software, and to permit persons to whom the Software is^furnished to do so, subject to the following conditions:^^The above copyright notice and this permission notice shall be included in all^copies or substantial portions of the Software.^^THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR^IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,^FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE^AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER^LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,^OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE^SOFTWARE.'))
+		
 		input("Press Return (Enter) to return to the Main Menu.\n> ")
+
+
 	return
 
 def twoKey():
+	global KEY_LENGTH
+	global MASK
+
+	if (MASK):
+		kOne = getpass.getpass("\nGive a Key, such as a word you will remember. Be sure it is not easily guessed: [Hidden]\n>  ")[0:KEY_LENGTH]
+	if (not MASK):
+		kOne = input("\nGive a Key, such as a word you will remember. Be sure it is not easily guessed:\n>  ")
 	
 	#Password generator
-	kOne = input("\nGive a Username Key. Be sure it is not easily guessed:\n> ")[0:12]
-	kTwo = input("\nGive a Site Key, such as a website name:\n> ")[0:12]
+	kTwo = input("\nGive a Site Key, such as a website name:\n> ")[0:KEY_LENGTH]
 	r = (genResult(kOne, kTwo))
 	print("\nResult:\n" + r + "\n")
 	#copyToclipPC(r)
@@ -52,9 +68,14 @@ def twoKey():
 	return
 
 def oneKey():
-	
+	kOne = ""
+	global KEY_LENGTH
+	global MASK
 	#Password generator
-	kOne = input("\nGive a Key, such as a word you will remember. Be sure it is not easily guessed:\n> ")[0:12]
+	if (MASK):
+		kOne = getpass.getpass("Give a Username Key: [Hidden]\n>  ")[0:KEY_LENGTH]
+	if (not MASK):
+		kOne = input("Give a Username Key: \n>  ")
 	kTwo = "Default_Key_2"
 	r = (genResult(kOne, kTwo))
 	print("\nResult:\n" + r + "\n")
@@ -175,6 +196,7 @@ LINE = 50
 KEY_LENGTH = 16
 PASSWORD_LENGTH = 16
 ENCODING = "utf_8" 
+MASK = True
 
 print("Loading Working Directory.")
 #set cwd to source file
@@ -184,19 +206,13 @@ try:
 	main(" ")
 
 except Exception:
-	traceback.print_exc()
+	#traceback.print_exc()
 	input("Error; press Return (Enter) to close.\n> ")
-
- #Sources:
- #Copy to Clipboard
-  #https://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
+'''
+SOURCES
+ Copy to Clipboard
+  https://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
  
- #Current Working Dir
-  #https://note.nkmk.me/en/python-os-getcwd-chdir/
-
- #Encryption
-  #https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
-
-  #Enbed via Trinket
-
-  # <iframe src="https://trinket.io/embed/python3/258ed5f022?outputOnly=true&runOption=run&start=result" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+ Current Working Dir
+  https://note.nkmk.me/en/python-os-getcwd-chdir/
+  '''
