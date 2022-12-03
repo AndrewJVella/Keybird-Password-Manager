3720708d21719b4a9eddafbbd6c9e9899f697343
PixelatedStarfish <68627998+PixelatedStarfish@users.noreply.github.com>
2022-12-02 22:50:49 -0500

    Update full_source.py
    Optimized and cleaned up edit mode help.
    Added a nano section to the docs as an isolated string that can be specifically printed to nano when "_help" is typed into edit mode. In the last version the entire doc was written to nano, which took about 10 seconds.

--- full_source.py
+++ full_source.py
@@ -6,6 +6,8 @@ import subprocess
 import sys
 import pwinput
 
+#closing nano with the red X leavs a .save file, these may need to be deleted from cwd
+
 #BASE MENU FUNCTIONS
 def main(d):
 
@@ -27,14 +29,13 @@ def main(d):
 	try:
 		global KEY_LENGTH
 		global PASSWORD_LENGTH
-		encrypt_to_file("Files/_help.txt", docs())
+		encrypt_to_file("Files/_help.txt", docs(False)) #get the Nano section of the docs only
 	except Exception:
 		pass
 	print("Loading Settings.")
 	getSettings()
 	fileCleaner()
 	print("Ready.\n----")
-	clear()
 	menu()
 	return
 
@@ -68,8 +69,9 @@ def getSettings():
 	return
 
 def menu():
+	clear()
 	while (True):
-		print("Thank you for using the Keybird Password Manager Prototype.\nCopyright 2022.\n\nThis is created by Andrew Vella.\nEmail:  andyjvella@gmail.com.\nGithub: @PixelatedStarfish\n\nPlease see the Disclaimer and License, before using this app.")
+		print("Thank you for using the Keybird Password Manager.\nCopyright 2022.\n\nThis is created by Andrew Vella.\nEmail:  andyjvella@gmail.com.\nGithub: @PixelatedStarfish\n\nPlease see the Disclaimer and License, before using this app.")
 		a = input("\nType a number and press enter (return) to select an option:\n0  Help\n1  One Key Mode\n2  Two Key Mode\n3  Username Generator\n4  File Menu\n5  Settings Menu\n6  Disclaimer and License\n7  Erase Data\n8  Close\n> ")
 		if (a == "-1"):
 			test()
@@ -97,7 +99,7 @@ def menu():
 			print("Disclaimer\n")
 			print(paragraphAlign("So, in plain terms, this app should be used with some care. It is about as secure as a basket of keys. If you keep it behind a locked door, your keys should stay put because only trusted people get anywhere near them. By analogy this app is the basket, and your computer is the door. You should be sure that your computer is secured and safe before using this app. No information produced in whole or part by this app is garenteed to be perfectly safe. Your files can be read, modified, or deleted. The app source code can also be read, modfied, or deleted, which would cause unpredictable effects while running the app. Back up important things; store your passwords in a few safe places. Keep untrustworthy people off your computer. Do not store your passwords publicly, or generate them with keys that are easy to guess. Stay safe!"))
 			print("\n\n")
-			print(paragraphAlign('MIT License^^Copyright (c) 2022 Andrew Vella^^Permission is hereby granted, free of charge, to any person obtaining a copy^of this software and associated documentation files (the "Software"), to deal^in the Software without restriction, including without limitation the rights^to use, copy, modify, merge, publish, distribute, sublicense, and/or sell^copies of the Software, and to permit persons to whom the Software is^furnished to do so, subject to the following conditions:^^The above copyright notice and this permission notice shall be included in all^copies or substantial portions of the Software.^^THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR^IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,^FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE^AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER^LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,^OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE^SOFTWARE.'))
+			print(paragraphAlign('MIT License^^Copyright (c) 2022 Andrew Vella^^Permission is hereby granted, free of charge, to any person obtaining a copy^of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:^^The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.^^THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR^IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,^FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER^LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'))
 		if (a == "7"):
 			clear()
 			h = YesOrNo("Erase all data and close? (y/n). This will end thr current session.\n")
@@ -379,7 +381,7 @@ def FileMenu(a):
 		if (a == "FileMenu"):
 			clear()
 			if (MASK):
-				print ("\n-Please note that all text in this menu is not masked or hidden. All file content will be plainly visible.")
+				print ("\n*Please note that all text in this menu is not masked or hidden. All file content will be plainly visible.*")
 
 			print("Files Menu\n\n0  Help\n1  List Files\n2  Read File\n3  Edit File\n4  Delete File\n5  Back to Main Menu\n6  Close.")
 			b = input("> ")
@@ -417,7 +419,7 @@ def fileSelector():
 	s = input("Please select a file by typing it's name. You do not need to include the extension.\nType nothing to return to the File Menu.\n>  ") + ".txt"
 	s = s.replace(".txt.txt", ".txt")
 
-	if (s == ""): #no option given
+	if (s == ".txt"): #no option given
 		FileMenu("FileMenu")
 
 	#edge case for edit mode
@@ -449,7 +451,12 @@ def printFileList():
 
 ###FORMATTING THAT ACTUALLY COOPERATES###
 def paragraphAlign(s): #for large strings
+	# ^ is newline
+
+	global LINE
+	s = s.replace("\n\n", "^^")
 	s = s.replace("\n", " ")
+	s = s.replace("  ", " ")
 	i = 0
 	j = 0
 	S = list(s)
@@ -578,172 +585,163 @@ def removeDuplicates(a):
 	return out
 
 #DOCS
+def docs(b = True):
+	# b allows for the printing of the entire doc, or just the nano section (b = False). When you type _help into edit mode, only the nano section should be loaded into nano in order to save time. 
+	nano = '''
+-EDIT MODE (HOW TO USE NANO)-
+Nano is a text editor that runs on linux. It can also run on the Windows Linux Subsystem. Keybird uses nano to edit
+the (unencrypted) contents of a file. The File Edit Mode opens the nano text editor after writing unencrypted file
+contents to the file "_temp.txt". You can type "_help" into Edit Mode in the File Menu to load this section of the
+documentation into nano. Edits to this document will not be saved, so you can experiment freely.
 
-def docs():
+A short guide to shortcuts:
+^G  means "press ctrl and G at the same time to use this shortcut" you can also press esc twice and then G
+M-G means "press alt and G at the same time to use this shortcut"
+
+Note that G can be substituted for any letter to get another shortcut. You should see a list of them at the bottom of
+this window. ^X exits the nano editor and returns to Keybird. For more information use ^G.
+
+Upon finishing your edit and exiting, you will be asked if you want to save your edit. Type "yes" or "y" to save your
+edit to the file you have selected. The file will be overwritten by the edit and encrypted. Type "no" or "n" to cancel
+the edit. 
+	'''
+
 	s= '''
 
-	-WELCOME TO KEYBIRD-
-	You need lots of passwords, but making them is tedious.
-	Repeating passwords reduces their effectiveness.
-	In-browser managers are fine until they lock you out. 
-	You may ask yourself:
-	"What was my password again?"
-	"Why does Safari fill in the wrong info?"
-	"Did I make this account on Google?"
-	Skip that nonsense, use Keybird.
+-WELCOME TO KEYBIRD-
+You need lots of passwords, but making them is tedious. Repeating passwords reduces their effectiveness. In-browser
+managers are fine until they lock you out. 
 
-	Keybird is designed to generate
-	and store passwords on your computer.
-	While other managers integrate with your browser,
-	they are much better for accumulating passwords
-	than actually managing them. At worst you can become
-	dependant on a specific browser to access your stuff.
-	At best you might still use a small number of passwords
-	you wrote yourself for the important stuff,
-	which puts the important stuff at risk.
-
-	My thinking is that 100 auto-generated passwords is bad,
-	a small number of manually created passwords is worse,
-	and both of these together is what often happens.
-	Instead of coming up with a few compilcated
-	passwords and using them everywhere,
-	depending on a browser extension,
-	or (most likely) both
+You may ask yourself: 
+"What was my password again?"
+"Why does Safari fill in the wrong info?" 
+"Did I make this account on Google?"
 	
-	This generates secure passwords from simple keys,
-	that are easy to create and remember.
+Skip that nonsense, use Keybird.
 
-	-FEATURES-
-	This app uses keys to make passwords.
-	The keys are easy to remeber.
-	The passwords are secure.
-	Keys are 16 characters max.
-	Passwords are 16 characters.
+Keybird is designed to generate and store passwords on your computer. While other managers integrate with your browser,
+they are much better for accumulating passwords than actually managing them. At worst you can become dependant on a 
+specific browser to access your stuff. At best you might still use a small number of passwords you wrote yourself for 
+the important stuff, which puts the important stuff at risk.
 
-	Two Key Mode.
-	One private key and many public keys.
-	The public keys can be website names.
-	You can have many passwords.
-	You only need to remeber one key.
+My thinking is that 100 auto-generated passwords is bad, a small number of manually created passwords is worse, and
+both of these together is what often happens. Instead of coming up with a few compilcated passwords and using them
+everywhere, depending on a browser extension, or (most likely) both. You can use Keybird to generate secure 
+passwords from simple keys, that are easy to create and remember.
 
-	Username Generator.
-	Helps you think of usernames. 
-	It asks questions, and generates names.
+-FEATURES-
+Keys:
+This app uses keys to make passwords. The keys are easy to remeber. The passwords are secure. 
+Keys are 16 characters max. 
+Passwords are 16 characters.
 
-	Files.
-	These store passwords and keys.
-	They are encrypted to prevent accidents.
-	They are not protected from app users.
-	Files are cleaned at start up.
-	Files are alphabatized by key.
-	Duplicate info is deleted.
+Two Key Mode:
+Keybird has a Two Key Mode. One private key and many public keys. The public keys can be website names. You can have
+many passwords and you only need to remeber one key.
 
-	Settings
-	Toggle file saving
-	Toggle masking, which covers private stuff.
-	Toggle debug mode, which prints errors.
+Username Generator:
+The Username Generator helps you think of usernames. It asks you three questions, and generates names from your
+answers.
 
-	Web Repl
-	Use the repl when you leave this app at home.
-	If you need a password quickly, use your key.
-	Nothing is saved on the repl.
+Files:
+Files store passwords and keys. They are encrypted to prevent accidents. Files are cleaned at start up. They are 
+alphabatized by key and duplicate info is deleted. Files can also be edited in the nano text editor.
 
+Settings:
+Toggle file saving
+Toggle masking, which covers private stuff.
+Toggle debug mode, which prints errors.
 
-	-INSTALL WSL-
-	#if you require the windows linux subsystem to run nano (a text editor) 
-	#run this script in batch
+Web Repl:
+Use the repl when you leave this app at home. If you need a password quickly, use your key. Nothing is saved on the
+repl.
 
-	echo WSL Installation
-	wsl --install
-	echo End of WSL Installation
+''' + nano + '''
 
+-INSTALL WSL-
+#if you require the windows linux subsystem to run nano (the text editor used by Keybird) 
+#run this command in Command Prompt by typing it in and pressing enter (return).
 
-	-GITHUB-
-	https://github.com/PixelatedStarfish/Keybird-Password-Manager
+wsl --install
 
-	-WEB REPL-
-	tinyurl.com/KeybirdWeb
+-GITHUB-
+https://github.com/PixelatedStarfish/Keybird-Password-Manager
 
-	-INDICATORS-
-	Note that a filename that begins with an underscore is reserved. 
-	Two underscores means the file is hidden.
-	^ is newline
-	> Indicates input
-	>> Masked input
+-WEB REPL-
+tinyurl.com/KeybirdWeb
 
-	-TESTS-
-	Tests are written into the source.
-	At the main menu select the following:
-	-1 for general testing purposes
-	-2 to run the file cleaner
-	-3 to generate sample files
+-INDICATORS-
+Note that a filename that begins with an underscore is reserved. 
+Two underscores means the file is hidden.
+> Indicates input
+>> Masked input
 
-	-LEGAL-
-	Disclaimer
-	So, in plain terms, this app should be used with some
-	care. It is about as secure as a basket of keys. If
-	you keep it behind a locked door, your keys should
-	stay put because only trusted people get anywhere near
-	them. By analogy this app is the basket, and your computer
-	is the door. You should be sure that your computer
-	is secured and safe before using this app. No information
-	produced in whole or part by this app is garenteed
-	to be perfectly safe. Your files can be read, modified,
-	or deleted. The app source code can also be read, modfied,
-	or deleted, which would cause unpredictable effects
-	while running the app. Back up important things; store
-	your passwords in a few safe places. Keep untrustworthy
-	people off your computer. Do not store your passwords
-	publicly, or generate them with keys that are easy
-	to guess. Stay safe!
+-TESTS-
+Tests are written into the source.
+At the main menu select the following:
+-1 for general testing purposes
+-2 to run the file cleaner
+-3 to generate sample files
 
-	MIT License
+-LEGAL-
+Disclaimer:
+So, in plain terms, this app should be used with some care. It is about as secure as a basket of keys. If you keep it
+behind a locked door, your keys should stay put because only trusted people get anywhere near them. By analogy this 
+app is the basket, and your computer is the door. You should be sure that your computer is secured and safe before 
+using this app. No information produced in whole or part by this app is garenteed to be perfectly safe. Your files 
+can be read, modified, or deleted. The app source code can also be read, modfied, or deleted, which would cause 
+unpredictable effects while running the app. Back up important things; store your passwords in a few safe places. 
+Keep untrustworthy people off your computer. Do not store your passwords publicly, or generate them with keys 
+that are easy to guess. Stay safe!
 
-	Copyright (c) 2022 Andrew Vella
+License:
+MIT License
 
-	Permission is hereby granted, free of charge, to any person obtaining 
-	a copy of this software and associated documentation files (the "Software"), 
-	to deal in the Software without restriction, including without limitation the 
-	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-	sell copies of the Software, and to permit persons to whom the Software is furnished 
-	to do so, subject to the following conditions:
+Copyright (c) 2022 Andrew Vella
 
-	The above copyright notice and this permission notice shall be included in all copies
-	or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining  a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
-	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
 
-	-SOURCES-
-	Clear Screen
-	https://www.csestack.org/clear-python-interpreter-console/
-	Copy to Clipboard
-	https://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
-	Encryption
-	https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
-	Get Current Working Directory
-	https://note.nkmk.me/en/python-os-getcwd-chdir/
- 	License
- 	https://choosealicense.com/
-	Nano
-	https://www.nano-editor.org/dist/latest/nano.html
-	Password Masking 
-	https://stackoverflow.com/questions/9202224/getting-a-hidden-password-input
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-	-NOTES-
-	Keybrid is written in Python3 and Batch for Windows.
-	Keybird is open source.
-	Developed by Andrew Vella
-	@PixelatedStarfish on Github and Itch
-	Copyright (c) 2022
-	Thank you for using Keybird!
+-SOURCES-
+Clear Screen
+https://www.csestack.org/clear-python-interpreter-console/
+Copy to Clipboard
+https://stackoverflow.com/questions/11063458/python-script-to-copy-text-to-clipboard
+Encryption
+https://www.geeksforgeeks.org/encoding-and-decoding-base64-strings-in-python/
+Get Current Working Directory
+https://note.nkmk.me/en/python-os-getcwd-chdir/
+License
+https://choosealicense.com/
+Nano
+https://www.nano-editor.org/dist/latest/nano.html
+Password Masking 
+https://stackoverflow.com/questions/9202224/getting-a-hidden-password-input
 
+-NOTES-
+Keybrid is written in Python3 and Batch for Windows.
+Keybird is open source.
+Developed by Andrew Vella
+Copyright (c) 2022
+@PixelatedStarfish on Github and Itch
+andyjvella@gmail.com
+Thank you for using Keybird!
 	'''
-	return s.replace("\t", "")
+	if (b):
+		return s.replace("\t", "")
+	if(not b):
+		return nano.replace("\t", "")
+
 
 #SYSTEM
 def copyToclipPC(txt):
@@ -772,7 +770,7 @@ def openNano(s):
 	f = open("_temp.txt", "w")
 	f.write(s)
 	f.close()
-	print("Loading Text Editor (Nano).")
+	print("Writing File to Text Editor (Nano).")
 	subprocess.run(["wsl", "nano","_temp.txt", "-t"])
 	f = open("_temp.txt", "r")
 	n = f.read()
@@ -792,7 +790,7 @@ def openNanoMac(s):
 	f = open("_temp.txt", "w")
 	f.write(s)
 	f.close()
-	print("Loading Text Editor (Nano).")
+	print("Writing File to Text Editor (Nano).")
 	subprocess.run(["nano","_temp.txt", "-t"])
 	f = open("_temp.txt", "r")
 	n = f.read()
@@ -912,7 +910,7 @@ print("Initializing.")
 SAVE = False
 DEBUG = False
 MASK = True
-LINE = 50
+LINE = 100
 KEY_LENGTH = 16
 PASSWORD_LENGTH = 16
 ENCODING = "utf_8" 
